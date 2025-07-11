0583d537baccba9ce981a325e8247119a6c904f1
joaomartinscode <jfbrancomartins@gmail.com>
2025-07-11 23:31:45 +0100

    create scoreboard component

---
+++ src/components/Scoreboard.jsx
@@ -0,0 +1,10 @@
+const Scoreboard = ({ score, bestScore }) => {
+	return (
+		<div className="scoreboard">
+			<p>Score: {score}</p>
+			<p>Best Score: {bestScore}</p>
+		</div>
+	);
+};
+
+export default Scoreboard;
